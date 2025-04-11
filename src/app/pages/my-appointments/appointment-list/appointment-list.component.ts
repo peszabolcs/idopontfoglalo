69dc5d973bffdb95fa1ff96806b6aaf325b8a84c
peszabolcs <perjesidev@gmail.com>
2025-04-11 19:40:18 +0200

    add more material elements

--- src/app/pages/my-appointments/appointment-list/appointment-list.component.ts
+++ src/app/pages/my-appointments/appointment-list/appointment-list.component.ts
@@ -1,13 +1,27 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Appointment } from '../../../models';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatChipsModule,
+    MatTooltipModule,
+  ],
 })
 export class AppointmentListComponent {
   @Input() appointments: Appointment[] = [];
